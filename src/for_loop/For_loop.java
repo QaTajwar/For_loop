5755e5b7bb3b6f5d6a184cda77f9574073c3245b
tajwar.hamid47@gmail.com <tajwar.hamid47@gmail.com>
2022-04-07 23:05:23 -0400

    first commit

---
+++ src/for_loop/For_loop.java
@@ -0,0 +1,25 @@
+package for_loop;
+
+public class For_loop {
+
+	public static void main(String[] args) {
+		
+		 
+		 
+	
+		 
+//		 for(int i=10; i>=0; i--) {                              
+//		System.out.println(i);
+//		}System.out.println("Happy New Year");
+//	}
+		for(int i=0; i<=10; i+=1) {
+			System.out.println(i);
+		}System.out.println("happy birthday");
+		}
+		
+	
+	
+	
+	
+
+}
